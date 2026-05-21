44ac62279c1bb3c922b81ecc1130f7e2f394f404
vijayz96 <vijuxpsy@gmail.com>
2026-05-21 22:46:01 +0530

    Add function to show active page

---
+++ script.js
@@ -0,0 +1,10 @@
+function showPage(pageId){
+
+  let pages = document.querySelectorAll('.page')
+
+  pages.forEach(page => {
+    page.classList.remove('active')
+  })
+
+  document.getElementById(pageId).classList.add('active')
+}
